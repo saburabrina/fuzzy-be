eed11fad1192286161fdc637ba73cf5deea60dac
saburabrina <sabrinascosme@gmail.com>
2024-03-20 15:00:07 -0300

    redis and sidekiq configuration

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module DesafioRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_name_prefix = "sidekiq_jobs"
   end
 end
